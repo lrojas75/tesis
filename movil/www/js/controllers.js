ce802627ecb050effd7fb737f5656d72a46c5ac5
Felipe <feliperojas12@hotmail.com>
2016-12-22 19:16:47 -0500

    Refactorizacion de los controladores, se agregan objetos para el manejo de la informacion de los formularios, ajustes en htmls

--- movil/www/js/controllers.js
+++ movil/www/js/controllers.js
@@ -1,71 +1,82 @@
-var app = angular.module('movilapp', ['ngMaterial']);
+var app = angular.module("movilapp", ['ngMaterial']);
 var ip = "http://192.168.0.15:80";
 
 //<<---------------------------------------------------------------------------------------------------------------------------------------->>
 //<<-------------------------------------------- Controlador para ventana de Informacion General-------------------------------------------->>
 //<<---------------------------------------------------------------------------------------------------------------------------------------->>
 app.controller("infoController", ['$scope', '$filter', '$http', function($scope, $filter, $http){
+    $scope.infoExiste=false;
+    $scope.infoFormData={
+        municipio:'',
+        barrio:'',
+        comuna:'',
+        actividad:'',
+        fecha: $filter('date')(Date.now(), 'dd-MM-yyyy')
+    };
+    $scope.changeView = function(nextPage,currentPage){
+        window.localStorage.setItem("previousPage", currentPage);
+        window.location.replace(nextPage);
+    };
 
-$scope.changeView = function(nextPage,currentPage){
-    window.localStorage.setItem("previousPage", currentPage);
-    window.location.replace(nextPage);
-}
-
-$scope.backNavigation = function() {
-    var previousPage = window.localStorage.getItem("previousPage");    
-    window.location.replace(previousPage);    
-}
+    $scope.backNavigation = function() {
+        var previousPage = window.localStorage.getItem("previousPage");    
+        window.location.replace(previousPage);    
+    };
 //<<--------------------- Funciones para ventana INFORMACION GENERAL ----------------------------------------->>
-$scope.opciones = [
+    $scope.opciones = [
     { municipio: "Cali", comunaNum: ["1", "2", "3", "4", "5", "6","7", "8", "9","10", "11","13", "17","19", "20", "21", "22"]},
     { municipio: "Palmira", comunaNum: ["1", "2", "3", "4", "5", "6","7", "8", "9","10", "11","13", "14", "15", "16"]},
     { municipio: "Cartago", comunaNum: ["1", "2", "3", "4", "5", "6","7"]}];
 
-$scope.fecha = $filter('date')(Date.now(), 'dd-MM-yyyy');
-$scope.infoExiste=false;
-//<------------------------------------FUNCION PARA AGREGAR LA INFORMACION GENERAL DEL RECORRIDO ------------------------->
-$scope.agregarInfoGeneral = function(){
-    if ($scope.municipio!=undefined && $scope.barrio!=undefined && $scope.comuna!=undefined && $scope.actividad!=undefined) {
+    
+    //<------------------------------------FUNCION PARA AGREGAR LA INFORMACION GENERAL DEL RECORRIDO ------------------------->
+    $scope.agregarInfoGeneral = function(){
+        console.log($scope.infoFormData.municipio.municipio);
+        console.log($scope.infoFormData.barrio);
+        console.log($scope.infoFormData.comuna);
+        console.log($scope.infoFormData.actividad);
+        if ($scope.infoFormData.municipio.municipio.trim()!='' && $scope.infoFormData.barrio.trim()!='' && $scope.infoFormData.comuna.trim()!='' && $scope.infoFormData.actividad.trim()!='') {}
         var user = window.localStorage.getItem("usuario"); 
         $http.post(ip+'/webApi.php?val=addInfoGeneral',{
             id:user,
-            municipio: $scope.municipio.municipio,
-            barrio: $scope.barrio,
-            comuna: $scope.comuna,
-            actividad: $scope.actividad,
-            fecha: $scope.fecha        
+            municipio: $scope.infoFormData.municipio.municipio,
+            barrio: $scope.infoFormData.barrio,
+            comuna: $scope.infoFormData.comuna,
+            actividad: $scope.infoFormData.actividad,
+            fecha: $scope.infoFormData.fecha        
         }).success(function(data) {
             window.localStorage.setItem("previousPage", "infoGeneral.html");
+            window.localStorage.setItem("municipio", $scope.infoFormData.municipio);
             window.location.replace("menuTipos.html");
         }).error(function(data) {
             alert("Error al ingresar los datos");
             console.log('Error: ' + data);
         });
-    }else{
-        alert("Datos");
-    }
-};
-    
-$scope.init = function(){
-    var user = window.localStorage.getItem("usuario"); 
-    $http.get(ip+'/webApi.php?val=checkInfoGeneral',{
-        params:{
-            usuario:user,
-            fecha:$scope.fecha
-        }
-    }).success(function(data){
-        if (data!=null) {
-            window.localStorage.setItem("infoID",data.id);
-            window.localStorage.setItem("previousPage", "index.html");
-            window.location.replace("menuTipos.html");
-        }else{
-            $scope.infoExiste=true;
-        }
-    }).error(function(data){
-        alert("Error al consultar los datos");
-    })
-}
-    
+    };
+
+    $scope.initInfo = function(){
+        var user = window.localStorage.getItem("usuario"); 
+        $http.get(ip+'/webApi.php?val=checkInfoGeneral',{
+            params:{
+                usuario:user,
+                fecha:$scope.infoFormData.fecha
+            }
+        }).success(function(data){
+            console.log(data);
+            if (data!=null) {
+                window.localStorage.setItem("infoID",data.id);
+                window.localStorage.setItem("municipio",data.municipio);
+                window.localStorage.setItem("previousPage", "index.html");
+                window.location.replace("menuTipos.html");
+            }else{
+                $scope.infoExiste=true;
+                console.log("init");
+            }
+        }).error(function(data){
+            alert("Error al consultar los datos");
+        })
+    };
+
 }]);
 
 //<<---------------------------------------------------------------------------------------------------------------------------------------->>
@@ -95,89 +106,138 @@ app.controller("menuController", ['$scope','$filter', '$http', function($scope, 
         }).error(function(data){
             alert("Error al consultar los datos");
         })
-    }
+    };
 
     //<<-----------------------Funciones para ventana MENU PRINCIPAL ---------------------------------------->>
-    function obtenerTotales(){ //Funcion que permite obtener el total de focos de infeccion agregados por cada tipo para mostrar el grafico
-
-    }
-
+    
     var sumideros = 25;
     var viviendas = 10;
     var cdh = 34;
     var campo = document.getElementById("grafico");
 
     Morris.Donut({
-              element: campo,
-              data: [
-                {label: "Sumidero", value: sumideros},
-                {label: "Vivienda", value: viviendas},
-                {label: "CDH", value: cdh},
-              ],
-              colors: ['#ffcd28', '#5d6865', '#f93e04']
+      element: campo,
+      data: [
+      {label: "Sumidero", value: sumideros},
+      {label: "Vivienda", value: viviendas},
+      {label: "CDH", value: cdh},
+      ],
+      colors: ['#ffcd28', '#5d6865', '#f93e04']
     }); //Cierra codigo de Donut.
 
     $scope.changeView = function(nextPage,currentPage){
         window.localStorage.setItem("previousPage", currentPage);
         window.location.replace(nextPage);
-    }
+    };
 }]);
 
 //<<---------------------------------------------------------------------------------------------------------------------------------------->>
 //<<-------------------------------------------- Controlador para ventana de Focos de infeccion-------------------------------------------->>
 //<<---------------------------------------------------------------------------------------------------------------------------------------->>
 app.controller("focoController", ['$scope', '$http', function($scope, $http){
+    $scope.municipios = [
+        { municipio: "Cali", comunaNum: ["1", "2", "3", "4", "5", "6","7", "8", "9","10", "11","13", "17","19", "20", "21", "22"]},
+        { municipio: "Palmira", comunaNum: ["1", "2", "3", "4", "5", "6","7", "8", "9","10", "11","13", "14", "15", "16"]},
+        { municipio: "Cartago", comunaNum: ["1", "2", "3", "4", "5", "6","7"]}];
+    //Valores del formulario de modificar info
+    $scope.editarInfo ={
+        municipio: window.localStorage.getItem("municipio"),
+        barrio:'',
+        comuna:'',
+        comunas:[],
+        actividad:''
+    }
+
+    $scope.editarInfo.comunas=$scope.municipios.filter(function( obj ) {
+          return obj.municipio == $scope.editarInfo.municipio;
+        })[0].comunaNum;
+
     $scope.backNavigation = function() {
         var previousPage = window.localStorage.getItem("previousPage");    
         window.location.replace(previousPage);    
-    }
+    };
     $scope.tipo='';
     //Funcion para activar el formulario que se va a mostrar
     $scope.mostrar = function(tipoEscogido){
         return $scope.tipo===tipoEscogido;
-    }
+    };
 
     $scope.changeView = function(nextPage,currentPage){
         window.localStorage.setItem("previousPage", currentPage);
         window.location.replace(nextPage);
+    };
+
+
+    $scope.modificarInfoGeneral = function(){
+        console.log($scope.editarInfo);
+        console.log("carajo");
+        if ($scope.editarInfo.municipio!='' && $scope.editarInfo.barrio!='' && $scope.editarInfo.comuna!='' && $scope.editarInfo.actividad!='') {
+            var infoID = window.localStorage.getItem("infoID"); 
+            console.log($scope.editarInfo);
+            // $http.post(ip+'/webApi.php?val=modInfoGeneral',{
+            //     idInfo:infoID,
+            //     barrio: $scope.editarInfo.barrio,
+            //     comuna: $scope.editarInfo.comuna,
+            //     actividad: $scope.editarInfo.actividad,
+            // }).success(function(data) {
+            //     window.localStorage.setItem("previousPage", "infoGeneral.html");
+            //     window.location.replace("menuTipos.html");
+            // }).error(function(data) {
+            //     alert("Error al ingresar los datos");
+            //     console.log('Error: ' + data);
+            // });
+        }
+    };
+
+
+    //Valores para el formulario de sumideros
+    $scope.sumideroForm={
+        estadoSumidero:'',
+        larvasSumidero:'',
+        pupasSumidero:'',
+        tratadoSumidero:'',
+        insecticidaSumidero:'',
+        cantInsecticidaSumidero:'',
+        ubicacionSumidero:''
     }
+
     $scope.agregarSumidero = function(){
         var idInfo=window.localStorage.getItem("infoID");
-        console.log($scope.estadoSumidero);
-        console.log($scope.tratadoSumidero);
-        console.log($scope.insecticidaSumidero);
-        console.log($scope.cantInsecticidaSumidero);
-        console.log($scope.ubicacionSumidero);
+        console.log($scope.sumideroForm.estadoSumidero);
+        console.log($scope.sumideroForm.tratadoSumidero);
+        console.log($scope.sumideroForm.insecticidaSumidero);
+        console.log($scope.sumideroForm.cantInsecticidaSumidero);
+        console.log($scope.sumideroForm.ubicacionSumidero);
 
-        if ($scope.estadoSumidero!=undefined && $scope.tratadoSumidero!=undefined && $scope.insecticidaSumidero!=undefined && $scope.cantInsecticidaSumidero!=undefined && $scope.ubicacionSumidero!=undefined) {
-            $http.post(ip+'/webApi.php?val=addSumidero',{
-                tipo:$scope.tipo,
-                estado: $scope.estadoSumidero,
-                larvas: $scope.larvasSumidero,
-                pupas: $scope.pupasSumidero,
-                tratado: $scope.tratadoSumidero,
-                insecticida: $scope.insecticidaSumidero,
-                cantidadInsecticida: $scope.cantInsecticidaSumidero,
-                idInfoGeneral: idInfo,
-                ubicacion: $scope.ubicacionSumidero
-            }).success(function(data) {
-                window.localStorage.setItem("previousPage", "menuTipos.html");
-                window.location.reload("focosView.html");
-            }).error(function(data) {
-                alert("Error al ingresar los datos");
-                console.log('Error: ' + data);
-            });
-        } else {
-            alert("No pueden haber campos vacios");
-        }    
-    }
+        // if ($scope.sumideroForm.estadoSumidero!='' && $scope.sumideroForm.tratadoSumidero!='' && $scope.sumideroForm.insecticidaSumidero!='' && $scope.sumideroForm.cantInsecticidaSumidero!='' && $scope.sumideroForm.ubicacionSumidero!='') {
+        //     $http.post(ip+'/webApi.php?val=addSumidero',{
+        //         tipo:$scope.tipo,
+        //         estado: $scope.estadoSumidero,
+        //         larvas: $scope.larvasSumidero,
+        //         pupas: $scope.pupasSumidero,
+        //         tratado: $scope.tratadoSumidero,
+        //         insecticida: $scope.insecticidaSumidero,
+        //         cantidadInsecticida: $scope.cantInsecticidaSumidero,
+        //         idInfoGeneral: idInfo,
+        //         ubicacion: $scope.ubicacionSumidero
+        //     }).success(function(data) {
+        //         window.localStorage.setItem("previousPage", "menuTipos.html");
+        //         window.location.reload("focosView.html");
+        //     }).error(function(data) {
+        //         alert("Error al ingresar los datos");
+        //         console.log('Error: ' + data);
+        //     });
+        // } else {
+        //     alert("No pueden haber campos vacios");
+        // }    
+    };
 
     $scope.agregarVivienda = function(){
 
-    }
+    };
     $scope.agregarCDH = function(){
 
-    }
+    };
 
     //<------------------------------------------FUNCION PARA GEOLOCALIZACION (API GEOLOCATION)--------------------------------->
     $scope.geolocation=function() {
@@ -192,7 +252,7 @@ app.controller("focoController", ['$scope', '$http', function($scope, $http){
             localStorage.setItem("latitude", lat);
             localStorage.setItem("longitude", lon);
 
-            $scope.ubicacionSumidero = lat+" - "+lon;
+            $scope.sumideroForm.ubicacionSumidero = lat+" - "+lon;
         }
         /*Esta funcion es llamada si existe un error en la geolocation*/
         function onError(error){
--- movil/www/js/userController.js
+++ movil/www/js/userController.js
@@ -5,45 +5,46 @@ app.controller("inicioController", ['$scope', '$http', function($scope, $http){
     $scope.existe = "";
     $scope.sesion = "";
 
-$scope.changeView = function(view){
-    window.location.replace(view);            
-}
+    $scope.changeView = function(view){
+        window.location.replace(view);            
+    };
 
 //<-----------------------FUNCION PARA INICIAR SESION ------------------------->
-$scope.login = function(){
-    $http.post(ip+'/webApi.php?val=loginUsuario',{
-        username: $scope.userLogin,
-        password: $scope.passLogin
-    }).success(function(data) {
-        window.localStorage.setItem("usuario", $scope.userLogin);
-        window.localStorage.setItem("previousPage", "index.html");
-        window.location.replace("infoGeneral.html");
-    }).error(function(data) {
-        console.log('Error: ' + data);
-        $scope.username="";
-        $scope.password="";
-        $scope.loginError = "Usuario y/o Contraseña invalidos.";
-    });
-}
+    $scope.login = function(){
+        $http.post(ip+'/webApi.php?val=loginUsuario',{
+            username: $scope.userLogin,
+            password: $scope.passLogin
+        }).success(function(data) {
+            window.localStorage.setItem("usuario", $scope.userLogin);
+            window.localStorage.setItem("previousPage", "index.html");
+            window.location.replace("infoGeneral.html");
+        }).error(function(data) {
+            console.log('Error: ' + data);
+            $scope.username="";
+            $scope.password="";
+            $scope.loginError = "Usuario y/o Contraseña invalidos.";
+        });
+    };
 
-//<-----------------------FUNCION PARA REGISTRAR UN USUARIO NUEVO ------------------------->
-$scope.registro = function(){
-    $http.post(ip+'/webApi.php?val=registroUsuario',{
-        cedula: $scope.cedula,
-        nombres: $scope.nombres,
-        apellidos: $scope.apellidos,
-        password: $scope.passRegistro
 
-    }).success(function(data) {
-        alert("Usuario registrado con exito!");
-        $scope.cedula = "";
-        $scope.nombres = "";
-        $scope.apellidos = "";
-        $scope.passRegistro = "";
-    }).error(function(data) {
-        console.log('Error: ' + data);
-        $scope.registerError = "No se pudo registrar al usuario.";
-    });
-}
+    //<-----------------------FUNCION PARA REGISTRAR UN USUARIO NUEVO ------------------------->
+    $scope.registro = function(){
+        $http.post(ip+'/webApi.php?val=registroUsuario',{
+            cedula: $scope.cedula,
+            nombres: $scope.nombres,
+            apellidos: $scope.apellidos,
+            password: $scope.passRegistro
+
+        }).success(function(data) {
+            alert("Usuario registrado con exito!");
+            $scope.cedula = "";
+            $scope.nombres = "";
+            $scope.apellidos = "";
+            $scope.passRegistro = "";
+        }).error(function(data) {
+            console.log('Error: ' + data);
+            $scope.registerError = "No se pudo registrar al usuario.";
+        });
+    };
 
 }]);
