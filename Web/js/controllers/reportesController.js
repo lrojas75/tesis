4593df616d0bc25d195842395c606399b4e2af8e
lrojas75 <feliperojas12@hotmail.com>
2017-04-25 19:15:16 -0500

    Tablas con reportes por mes

--- Web/js/controllers/reportesController.js
+++ Web/js/controllers/reportesController.js
@@ -37,39 +37,67 @@ angular.module('app').controller('reportesController', function($scope, $http, $
 	$scope.filtrarPorMes = function(){
 		if($scope.todos.length > 0){
 			//Sumideros
-			var temp = [];
-			for (i = 0; i < $scope.todos[0].length; i++) {
-				var split = $scope.todos[0][i].fecha.split("-");
-				console.log(split);
-				var fechaFoco = new Date(split[2], split[1], split[0]);
-				$scope.mesFoco = fechaFoco.getMonth();
-				if($scope.mesFoco == $scope.mesusuario){
-					temp.push($scope.todos[0][i]);
+			$scope.sumideros = $scope.todos[0];
+			if($scope.sumideros.length > 0){
+				$scope.temp = [];
+				for (i = 0; i < $scope.sumideros.length; i++) {
+					var split = $scope.sumideros[i].fecha.split("-");
+					var fechaFoco = new Date(split[2], split[1], split[0]);
+					$scope.mesFoco = fechaFoco.getMonth();
+					console.log($scope.mesFoco+" - "+$scope.mesusuario);
+					if($scope.mesFoco == $scope.mesusuario){
+						$scope.temp.push($scope.sumideros[i]);
+						console.log(true);
+					}
 				}
+				$scope.focosReporteSumidero = $scope.temp;
+			}else{
+				$scope.focosReporteSumidero = [{"Municipio":"Cali","cantidad":0,"positivos":0,"tratados":0,"insecticida":"-","totalinsecticida":0},
+				{"Municipio":"Palmira","cantidad":0,"positivos":0,"tratados":0,"insecticida":"-","totalinsecticida":0},
+				{"Municipio":"Yumbo","cantidad":0,"positivos":0,"tratados":0,"insecticida":"-","totalinsecticida":0}];
 			}
-			$scope.focosReporteSumidero = temp;
+			
 			//Viviendas
-			var temp = [];
-			for (i = 0; i < $scope.todos[1].length; i++) {
-				var split = $scope.todos[1][i].fecha.split("-");
-				var fechaFoco = new Date(split[2], split[1], split[0]);
-				$scope.mesFoco = fechaFoco.getMonth();
-				if($scope.mesFoco == $scope.mesusuario){
-					temp.push($scope.todos[1][i]);
+			$scope.viviendas = $scope.todos[1];
+			if($scope.viviendas.length > 0){
+				$scope.temp = [];
+				for (i = 0; i < $scope.viviendas.length; i++) {
+					var split = $scope.viviendas[i].fecha.split("-");
+					console.log(split);
+					var fechaFoco = new Date(split[2], split[1], split[0]);
+					$scope.mesFoco = fechaFoco.getMonth();
+					if($scope.mesFoco == $scope.mesusuario){
+						$scope.temp.push($scope.viviendas[i]);
+					}
 				}
+				$scope.focosReporteVivienda = $scope.temp;
+			}else{
+				$scope.focosReporteVivienda = [{"Municipio":"Cali","cantidad":0,"depositos":0,"tratados":0,"eliminados":0,"larvas":0,"insecticida":0},
+				{"Municipio":"Palmira","cantidad":0,"depositos":0,"tratados":0,"eliminados":0,"larvas":0,"insecticida":0},
+				{"Municipio":"Yumbo","cantidad":0,"depositos":0,"tratados":0,"eliminados":0,"larvas":0,"insecticida":0}];
 			}
-			$scope.focosReporteVivienda = temp;
-			//CDH
-			var temp = [];
-			for (i = 0; i < $scope.todos[2].length; i++) {
-				var split = $scope.todos[2][i].fecha.split("-");
-				var fechaFoco = new Date(split[2], split[1], split[0]);
-				$scope.mesFoco = fechaFoco.getMonth();
-				if($scope.mesFoco == $scope.mesusuario){
-					temp.push($scope.todos[2][i]);
+			
+
+			//CDHs
+			$scope.cdh = $scope.todos[2];
+			if($scope.cdh.length > 0){
+				$scope.temp = [];
+				for (i = 0; i < $scope.cdh.length; i++) {
+					var split = $scope.cdh[i].fecha.split("-");
+					console.log(split);
+					var fechaFoco = new Date(split[2], split[1], split[0]);
+					$scope.mesFoco = fechaFoco.getMonth();
+					if($scope.mesFoco == $scope.mesusuario){
+						$scope.temp.push($scope.cdh[i]);
+					}
 				}
+				$scope.focosReporteCDH = $scope.temp;
+			}else{
+				$scope.focosReporteCDH = [{"Municipio":"Cali","cantidad":0,"depositos":0,"tratados":0,"eliminados":0,"larvas":0,"insecticida":0},
+				{"Municipio":"Palmira","cantidad":0,"depositos":0,"tratados":0,"eliminados":0,"larvas":0,"insecticida":0},
+				{"Municipio":"Yumbo","cantidad":0,"depositos":0,"tratados":0,"eliminados":0,"larvas":0,"insecticida":0}];
 			}
-			$scope.focosReporteCDH = temp;
+			
 		}else{
 			$scope.focosReporteSumidero = [{"Municipio":"Cali","cantidad":0,"positivos":0,"tratados":0,"insecticida":"-","totalinsecticida":0},
 				{"Municipio":"Palmira","cantidad":0,"positivos":0,"tratados":0,"insecticida":"-","totalinsecticida":0},
@@ -97,7 +125,6 @@ angular.module('app').controller('reportesController', function($scope, $http, $
 					$scope.mesusuario = date.getMonth()+1; //obtiene el mes actual
 				}
 				$scope.todos = data;
-				
 				// $scope.dataFiltrada = $scope.filtrarPorMes(data[0]);
 				// console.log($scope.dataFiltrada);
 				// if($scope.dataFiltrada.length > 0){
